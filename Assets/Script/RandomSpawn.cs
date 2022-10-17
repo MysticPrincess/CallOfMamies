8e8cad8a60601917431ca29571a67aaa7a2eef42
unknown <admin@GA211-22-12.ad.univ-angers.fr>
2022-10-17 08:25:56 +0200

    11/10

---
+++ Assets/Script/RandomSpawn.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RandomSpawn : MonoBehaviour
+{
+    
+   /* public GameObject[] lespositions;
+    public GameObject prefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //int [] RandomIndice = new int[5];
+        List<int> indice = new List<int>();
+        HashSet<int> hashset = new HashSet<int>();
+        //int size = 0; 
+        indice.Add(Random.Range(0, 10));
+        int temp = 0;
+
+        //Debug.Log("before la boucle");
+        for (int i = 0; i < 5; i++)
+        {
+            int flag = 0;
+            //Debug.Log("indice i=");
+            //Debug.Log(i);           
+            int ok = 0;
+
+            while (ok != 1)
+            {
+                temp = Random.Range(0, 10);
+                for (int j = 0; j < indice.Count; j++)
+                {
+                       /* if (temp== indice[j])
+                        {
+                            flag = 1;
+                            j = indice.Count;
+
+                        }*/
+            /*    }
+
+
+                    if (flag == 0)
+                    {
+                        ok = 1;
+                        indice.Add(temp);
+                    }
+             }
+
+
+
+                //  Debug.Log(temp);
+
+            }
+
+
+
+            //transform.position = lespositions[randomNb].transform.position;
+            //Instantiate(prefab, lespositions[randomNb].transform.position, Quaternion.identity);
+
+
+        }
+            */
+
+    }
+
